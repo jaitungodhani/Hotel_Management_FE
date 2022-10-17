f53de1d6493669a194307bc864eb09806788c56f
Jaitun Godhani <62869116+jaitungodhani@users.noreply.github.com>
2022-10-17 17:46:29 +0530

    add drawer in manager page

--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -2,6 +2,7 @@ import { Grid } from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Order from "../components/Order";
+import PersistentDrawerLeft from "../components/RightDrawer";
 import { managerorderStart } from "../redux/actions";
 
 
@@ -13,7 +14,8 @@ const Manager = () => {
   },[]);
   return (
     <div>
-      <Grid container>
+      <PersistentDrawerLeft />
+      <Grid container sx={{mt:9}}>
         {orders.map((i) => (
           <Grid i xs={12} sm={6} md={4} lg={4}>
             <Order particular_order={i} />
